8ad624d88acb59701e9dea111d2139fe5cf652af
Julian Calicis <Julian.calicis@protonmail.com>
2026-08-19 11:27:58 +0200

    refactor(MazePosition): add class to represent positions

---
+++ Model/MazePosition.cs
@@ -0,0 +1,26 @@
+﻿namespace Model
+{
+  public class MazePosition : IComparable<MazePosition>
+  {
+    //TODO: permettre à MazePosition de savoir le nombre de colonnes dans le labyrinthe pour donner une signification à la valeur de comparaison.
+    public MazePosition(int row, int column)
+    {
+      Row = row;
+      Column = column;
+    }
+
+    public int Row { get; }
+    public int Column { get; }
+
+    public int CompareTo(MazePosition? other)
+    {
+      if (other != null)
+        throw new ArgumentNullException(nameof(other));
+      if (Row > other.Row) return 1;
+      else if (Row < other.Row) return -1;
+      else if (Column > other.Column) return 1;
+      else if (Column < other.Column) return -1;
+      else return 0;
+    }
+  }
+}
