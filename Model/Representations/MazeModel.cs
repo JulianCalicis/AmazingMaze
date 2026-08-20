ff666d03ea404c69a34abc586102b863e4c32d2c
Julian Calicis <Julian.calicis@protonmail.com>
2026-08-20 18:09:11 +0200

    feat: add multiple player playability
    todo: character item exchange, character selection, character bag display
    note: characters get added through the Model Indexer, wow

--- Model/Representations/MazeModel.cs
+++ Model/Representations/MazeModel.cs
@@ -13,9 +13,44 @@ namespace Model.Representations
 
     private SortedDictionary<MazePosition, IDisplayable> _grid;
 
+    /// <summary>
+    /// Represents each character linked to its representation symbol.
+    /// </summary>
+    /// <remarks>Corresponds to PersonagesMap</remarks>
+    private Dictionary<char, Character> _characters;
+
+    /// <summary>
+    /// Represents the representation symbols of characters still present in the maze.
+    /// </summary>
+    private List<char> _characterKeys;
+
+    private int _activeCharacter;
+
     #endregion Fields
 
     #region Properties
+
+    /// <summary>
+    /// Activates the next character using the list of representation symbols
+    /// </summary>
+    public void ActivateCharacter()
+    {
+      _activeCharacter++;
+      if (_activeCharacter >= _characterKeys.Count)
+        _activeCharacter = 0;
+    }
+
+    /// <summary>
+    /// Activates a the character represented by <paramref name="representation"/>
+    /// </summary>
+    /// <param name="representation"></param>
+    /// <exception cref="ArgumentException"></exception>
+    public void ActivateCharacter(char representation)
+    {
+      int index = _characterKeys.IndexOf(representation);
+      if (index < 0) throw new ArgumentException("Character doesn't exist");
+      _activeCharacter = index;
+    }
 
     /// <summary>
     /// Name of the maze.
@@ -29,7 +64,7 @@ namespace Model.Representations
     /// Impossible de mettre la propriété en lecture seule si elle doit être initialisée par autre chose que le
     /// constructeur, dans ce cas, elle doit être initialisée par le setter de l'indexeur
     /// </remarks>
-    public Character Player { get; private set; }
+    public Character Player => _characters[_characterKeys[_activeCharacter]];
 
     /// <summary>
     /// Indexer that allows direct grid access using a <see cref="MazePosition"/>
@@ -50,10 +85,10 @@ namespace Model.Representations
             throw new Exception("The newRoom is already occupied.");
           if (value is Character valueCharacter)
           {
-            if (Player != null)
+            if (_characters.ContainsKey((char)valueCharacter.Representation))
               throw new Exception("Player already exists");
-
-            Player = valueCharacter;
+            _characters.Add((char)valueCharacter.Representation, valueCharacter);
+            _characterKeys.Add((char)valueCharacter.Representation);
           }
           room.Content = valuePlaceable;
         }
@@ -69,6 +104,9 @@ namespace Model.Representations
     public MazeModel(string name)
     {
       _grid = new SortedDictionary<MazePosition, IDisplayable>();
+      _characterKeys = new List<char>();
+      _characters = new Dictionary<char, Character>();
+      _activeCharacter = 0;
       Name = name;
     }
 
@@ -77,12 +115,24 @@ namespace Model.Representations
     public void Move(MovementDirection direction)
     {
       if (Player == null) throw new Exception("Le personnage n'existe pas");
-      MazePosition NewPos = (Player.Position?[direction]) ?? throw new Exception("Le personnage est sorti du labyrinthe");
-      if (this[NewPos] is not Room newRoom) throw new Exception("Le personnage essaie d'aller dans autre chose qu'une pièce");
+      MazePosition? NewPos = (Player.Position?[direction]);
+      if (NewPos == null || !_grid.ContainsKey(NewPos))
+      {
+        ((Room)this[Player.Position]).Content = null;
+        Player.Position = null;
+        _characterKeys.Remove((char)Player.Representation);
 
-      ((Room)this[Player.Position]).Content = null;
-      newRoom.Visite(Player);
-      Player.Position = NewPos;
+        //throw new Exception("Le personnage est sorti du labyrinthe");
+        if (_characterKeys.Count <= 0) throw new Exception("Partie Terminée");
+      }
+      else
+      {
+        if (this[NewPos] is not Room newRoom) throw new Exception("Le personnage essaie d'aller dans autre chose qu'une pièce");
+
+        ((Room)this[Player.Position]).Content = null;
+        newRoom.Visite(Player);
+        Player.Position = NewPos;
+      }
     }
 
     public IEnumerator<IDisplayable> GetEnumerator()
